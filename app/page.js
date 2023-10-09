e3b1cb3eeb76ed91e4116f3994124d3f06454329
mahmoudamr1 <mahmoudamr700@gmail.com>
2023-10-09 23:22:46 +0300

    two

--- app/page.js
+++ app/page.js
@@ -7,10 +7,84 @@ import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 import { ImSpinner9 } from 'react-icons/im';
 import Accordion from './According';
 import CountdownTimer from './CountDown';
-
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function Home() {
 
+
+  const [index, setIndex] = useState( 0 );
+  const images = [
+
+    '/img2.avif',
+    '/img1.avif',
+    '/img3.avif',
+    '/img4.avif',
+    '/img5.avif',
+    '/img6.avif',
+    '/img7.avif',
+    '/img8.jpg',
+    '/img9.jpg',
+    '/img10.jpg',
+    '/img11.jpg',
+    '/img12.jpg',
+    '/img13.jpg',
+    '/img14.jpg',
+    '/img15.jpg',
+    '/img16.jpg',
+    '/img17.jpg',
+  ];
+
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 4000,
+    cssEase: "linear",
+    pauseOnHover: true
+    , responsive: [
+      {
+        breakpoint: 1024, // تغيير التكوين عند وصول عرض الشاشة إلى 1024 بكسل
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 992, // تغيير التكوين عند وصول عرض الشاشة إلى 1024 بكسل
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 768, // تغيير التكوين عند وصول عرض الشاشة إلى 768 بكسل
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 577, // تغيير التكوين عند وصول عرض الشاشة إلى 768 بكسل
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 455, // تغيير التكوين عند وصول عرض الشاشة إلى 768 بكسل
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
 
 
   const [loading, setLoading] = useState( false );
@@ -156,6 +230,12 @@ export default function Home() {
           <h3>Sınırlı bir süre için, Kadın Barbie Spor Takımı koleksiyonunu yalnızca TRY249.00 dan başlayan <br/> fiyatlarla elde edebilirsiniz! Bu inanılmaz fiyatlarla harika bir görünüme sahip olma fırsatını kaçırmayın.</h3>
           <h4>Sezonun modasına katılın ve Kadın Barbie Spor Takımı koleksiyonu ile farkınızı ortaya koyun!</h4>
           <h5>Sezonun modasına katılın ve Kadın Barbie Spor Takımı koleksiyonu ile farkınızı ortaya koyun!</h5>
+         
+          <div className='prices'>
+            <div> <h4>TRY249.00</h4></div>
+            <div> <h5> TRY599.00</h5></div>
+          </div>
+         
           <div className='go-buy-button'>
             <a href="#form">Sepete Ekle</a>
           </div>
@@ -178,6 +258,10 @@ export default function Home() {
 
         <div className='sec3 py-7'>
           <h2>Sınırlı bir süre için, Kadın Barbie Oversize Eşofman Takımı koleksiyonunu %40 İNDİRİM İLE edinin!</h2>
+          <div className='prices'>
+            <div> <h4>TRY249.00</h4></div>
+            <div> <h5> TRY599.00</h5></div>
+          </div>
           <div className='go-buy-button'>
             <a href="#form">Sepete Ekle</a>
           </div>
@@ -290,8 +374,11 @@ export default function Home() {
 
             </video>
           </div>
+          
         </div>
-
+        <div className='go-buy-button pb-6 text-xl'>
+          <a href="#form">Sepete Ekle</a>
+        </div>
 
 
 
@@ -305,8 +392,32 @@ export default function Home() {
         </div>
 
 
+        <div className='sec9 container'>
+          <Slider
+            { ...settings }
+            className="image-container"
+            beforeChange={ ( oldIndex, newIndex ) => setIndex( newIndex ) }
+          >
+            { images.map( ( image, i ) => (
+              <Image
+                src={ image  }
+                alt="product image"
+                width={ 800 }
+                height={ 1200 }
+                key={ i }
+                priority={ true }
+                quality={ 100 }
+                blurDataURL={ `data:img2/avif` }
+                placeholder='blur'
+                loading='eager'
+
+              //  fill
+              />
+            ) ) }
+          </Slider>
+        </div>
         
-        <div className='sec-8 container'>
+        <div className='sec-8 container py-6'>
 
 
           <form
